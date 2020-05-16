c52c382c8dcda2a790185e74c9470b1a85c53b6c
LEVEQUE <alexis.leveque78@gmail.com>
2020-05-16 12:55:18 +0200

    Update Subscribe form

--- P5_01_projet/Public/View/Subscribe.php
+++ P5_01_projet/Public/View/Subscribe.php
@@ -7,15 +7,42 @@ ob_start();
 
 
 ?>
-    <fieldset>
-        <form action="index.php?action=triloualaoudadhefgzi" method="post">
-            <input type="text" placeholder="un champ">
-            <input type="text" placeholder="un champ">
-            <input type="text" placeholder="un champ">
-            <input type="text" placeholder="un champ">
-            <input type="submit" class="btn btn-primary">
-        </form>
-    </fieldset>
+    <div class="row">
+        <div class="col-lg-8 mb-4">
+            <h3>Inscription</h3>
+            <form name="addUser" id="userForm" method="post" action="../index.php?action=addUser" >
+                <div class="control-group form-group">
+                    <div class="controls">
+                        <label>Nom d'utilisateur:</label>
+                        <input type="text" class="form-control" id="user_name" required data-validation-required-message="Entrez votre nom d'utilisateur.">
+                        <p class="help-block"></p>
+                    </div>
+                </div>
+                <div class="control-group form-group">
+                    <div class="controls">
+                        <label>E-mail:</label>
+                        <input type="email" class="form-control" id="email" required data-validation-required-message="Entrez votre E-mail.">
+                    </div>
+                </div>
+                <div class="control-group form-group">
+                    <div class="controls">
+                        <label>Mot de passe : </label>
+                        <input type="password" class="form-control" id="password" required data-validation-required-message="Entrez un mot de passe.">
+                    </div>
+                </div>
+                <div class="control-group form-group">
+                    <div class="controls">
+                        <label>Confirmation du mot de passe : </label>
+                        <input type="password" class="form-control" id="password_confirmation" required data-validation-required-message="Entrez la confirmation du mot de passe." >
+                    </div>
+                </div>
+                <div id="success"></div>
+                <!-- For success/fail messages -->
+                <button type="submit" class="btn btn-primary" id="sendMessageButton">Send Message</button>
+            </form>
+        </div>
+    </div>
+
 
 
 
