a7fb2bc03d0944def4dd986f60d3d1f1d75b94e1
LEVEQUE <alexis.leveque78@gmail.com>
2020-05-26 11:03:38 +0200

    commit before mentoring

--- P5_01_projet/App/Entity/Entity.php
+++ P5_01_projet/App/Entity/Entity.php
@@ -8,6 +8,7 @@ class Entity
     public function hydrate(array $data)
     {
         foreach ($data as $key => $value) {
+            $key = str_replace('_',ucfirst(''),$key);
             $method = 'set' . ucfirst($key);
             if (method_exists($this, $method)) {
                 $this->$method($value);
